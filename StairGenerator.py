612c712ea33d957b5b9b8ca342fec5aec9482997
zhanrnl <Lennart@.(none)>
2011-07-18 15:52:26 -0700

    Progress on generate_stairs

--- StairGenerator.py
+++ StairGenerator.py
@@ -8,7 +8,7 @@
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
 
-import re
+from numpy import array, cross
 from collections import OrderedDict
 
 class StairGenerator():
@@ -21,9 +21,18 @@ class StairGenerator():
     vmf_tokens = []
     vmf_dict = OrderedDict()
 
+    # default stair params (not used yet, we're only making ramps atm)
+    step_size = 12, 8 # length, height
+    trim_width = 2
+    construction = 0 # 0 = solid, 1 = hanging, 2 = hanging step
+
     def __init__(self, vmf_filename):
         self.vmf_filename = vmf_filename
-        vmf_file = open(self.vmf_filename, 'r')
+        try:
+            vmf_file = open(self.vmf_filename, 'r')
+        except:
+            print "No file found with that filename!"
+            quit()
         self.vmf_data = vmf_file.read()
         self.tokenize_vmf()
         self.dictify_vmf()
@@ -66,6 +75,8 @@ class StairGenerator():
         """Helper for write_vmf"""
         output = ""
         for k, v in iter(subdict.iteritems()):
+            if k[0] == '*':
+                pass # internal data, don't write to file
             if type(v) is OrderedDict:
                 output += ('\t'*indent + k + '\n' + '\t'*indent + '{\n'
                     + self.write_vmf_level(v, indent+1) + '\t'*indent
@@ -78,6 +89,49 @@ class StairGenerator():
             else:
                 output += '\t'*indent + '"' + str(k) + '" "' + str(v) + '"\n'
         return output
+
+    def generate_stairs(self):
+        templates = self.find_templates()
+        print len(templates), "template%s found" % ("" if (len(templates) == 1)
+            else "s")
+        for template_num, template in enumerate(templates):
+            print "\nGenerating stairs for template #", template_num+1
+            sides = template['side']
+            print "Adding normals to vmf data"
+            self.add_normals(sides)
+            front_face = [side for side in sides if side['material'] ==
+                'SIGNS/STAIRS_RED'][0]
+            front_normal = front_face['*normal']
+            back_normal = [-num for num in front_normal]
+            back_face = [side for side in sides if side['*normal']
+                == back_normal][0]
+            if front_normal == [1, 0, 0]:
+                print "Template faces east"
+            elif front_normal == [0, 1, 0]:
+                print "Template faces north"
+            elif front_normal == [-1, 0, 0]:
+                print "Template faces west"
+            elif front_normal == [0, -1, 0]:
+                print "Template faces south"
+            else:
+                raise Exception('Template has bad normal')
+
+    def add_normals(self, sides):
+        """Given a list of vmf side dicts, adds normals to each side under
+        ['*normals']."""
+        for side in sides:
+            # figure out direction of normal
+            coords = [array(coord) for coord in
+                self.parse_coord_list(side['plane'])]
+            vec = [] # Gah, damn you vmf, ordering points in CW order, you
+            vec.append(coords[1] - coords[0]) # think using LHR is funny?
+            vec.append(coords[0] - coords[2])
+            side['*normal'] = [int(num/abs(num)) if num != 0 else 0 for num in
+                list(cross(vec[0], vec[1]))]
+            if side['*normal'][2] != 0 and (side['material'] ==
+                'SIGNS/STAIRS_RED'):
+                print "Can't have STAIRS_RED on top or bottom face"
+                templates.remove(templates.index(template))
 
     def find_templates(self):
         """Finds stairs template brushes in vmf, checks based on orientation and
@@ -115,8 +169,7 @@ class StairGenerator():
     def side_ortho(self, side):
         """Returns whether all points in a side lie in an orthogonal plane"""
         # turn into list of coordinate tuples
-        coords = [tuple(coord.split(' ')) for coord in
-            side['plane'][0:-1].replace('(', '').split(') ')]
+        coords = self.parse_coord_list(side['plane'])
         all_same = [0]*3
         # all three points lie in axial-oriented orthogonal plane if one coord
         # is same for all points
@@ -125,10 +178,16 @@ class StairGenerator():
                 and coords[0][i] == coords[2][i])
         return sum(all_same)
 
+    def parse_coord_list(self, coords):
+        """Breaks up the vmf syntax for 3 sets of coordinates into Python data
+        types"""
+        return [tuple([float(num) for num in coord.split(' ')]) for coord in
+            coords[0:-1].replace('(', '').split(') ')]
+
 
 def main():
-    generator = StairGenerator("anglestest.vmf")
-    generator.find_templates()
+    generator = StairGenerator("stairstest.vmf")
+    generator.generate_stairs()
 
 if __name__ == '__main__':
     main()
